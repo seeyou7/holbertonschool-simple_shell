083df4aeb5f8c8ed5d137a7a4e421f0fa97cc33c
seeyou7 <>
2023-08-20 05:52:05 -0700

    main.h file

---
+++ shell.h
@@ -0,0 +1,36 @@
+#ifndef __SIMPLE_SHELL__
+#define __SIMPLE_SHELL__
+
+#define IN 1
+#define OUT 0
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
