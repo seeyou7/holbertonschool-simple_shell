d71baf0945dc113584bc0d5dcd027a3eb23888c4
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-08-21 05:05:28 -0700

    revise the main function of the shell program

--- simple_shell.c
+++ simple_shell.c
@@ -1,7 +1,6 @@
 #include "shell.h"
 /**
  * main - entry point of the program
- *
  * Return: 0 Success
  */
 int main(void)
@@ -16,18 +15,17 @@ int main(void)
 		{"env", print_env},
 		{NULL, NULL}
 	};
-
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 		{
-			write(STDOUT_FILENO, "new_shell>>", 11);
+			write(STDOUT_FILENO, "new_shell>> ", 12);
 		}
 		input_cmd = read_command();
-		arr_cmd = split_input(input_cmd);
+		arr_cmd = split_cmd(input_cmd);
 		while (builtins[i].cmd != NULL)
 		{
-			if (strcmp(builtins[i].cmd, input_cmd) == 0)
+			if (strcmp(builtins[i].cmd, arr_cmd[0]) == 0)
 			{
 				builtins[i].func(arr_cmd);
 				true_builtin = 1;
@@ -41,10 +39,11 @@ int main(void)
 			{
 				execute_cmd(arr_cmd);
 			}
-				free(input_cmd);
-				free(arr_cmd);
-			}
+			free(input_cmd);
+			free(arr_cmd);
 		}
-		return (0);
+		true_builtin = 0;
+		i = 0;
 	}
+	return (0);
 }
