04780abe0e7d8c48eafb7649c038e70fe0caf553
seeyou7 <>
2023-08-22 08:08:50 -0700

    add the split env func

--- helper_functions.c
+++ helper_functions.c
@@ -16,3 +16,33 @@ char *get_env(char *input_cmd)
 	}
 	return (NULL);
 }
+#include "shell.h"
+
+/**
+  * split_env - tokenize the path
+  * @path: path to tokenize
+  * Return: array of tokens
+  */
+char **split_env(char *path)
+{
+        int size = 1024;
+        int i = 0;
+        char **arr_path = malloc(size * sizeof(char *));
+        char *token;
+
+        if (arr_path == NULL)
+        {
+                perror("Error : malloc");
+                exit(1);
+        }
+        token = strtok(path, ":");
+        while (token != NULL)
+        {
+                arr_path[i] = token;
+                i++;
+        	token = strtok(NULL, ":");
+        }
+        arr_path[i] = NULL;
+        return (arr_path);
+}
+~
