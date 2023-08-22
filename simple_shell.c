f081e5ea15f97164f691a428120e0c234ac3f27c
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-08-22 08:03:12 -0700

    update the main function

--- simple_shell.c
+++ simple_shell.c
@@ -3,31 +3,45 @@
  * main - entry point of the program
  * Return: 0 Success
  */
-int main(void)
+int main()
 {
 	char **arr_cmd;
 	char *input_cmd;
 	int i = 0;
 	int true_builtin = 0;
+	int interactive_mode = 0;
 
 	builtin_t builtins[] = {
 		{"exit", exit_func},
 		{"env", print_env},
 		{NULL, NULL}
 	};
+
+	if (isatty(STDIN_FILENO) == 1)
+		interactive_mode = 1;
+
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
+		if (interactive_mode)
 		{
-			write(STDOUT_FILENO, "new_shell>> ", 12);
+			write(STDOUT_FILENO, "$shell>> ", 9);
 		}
+
 		input_cmd = read_command();
+		if (input_cmd == NULL)
+		{
+			if (!interactive_mode)
+			{
+				break;
+			}
+			continue;
+		}
 		arr_cmd = split_cmd(input_cmd);
 		while (builtins[i].cmd != NULL)
 		{
 			if (strcmp(builtins[i].cmd, arr_cmd[0]) == 0)
 			{
-				builtins[i].func(arr_cmd);
+				builtins[i].func();
 				true_builtin = 1;
 				break;
 			}
